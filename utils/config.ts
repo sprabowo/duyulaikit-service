8ac008a30337b3fb1ea38cd7c8799f5f0a74f258
Sigit Prabowo <sigit.prabowo@go-jek.com>
2021-08-01 08:26:53 +0700

    fix url

--- utils/config.ts
+++ utils/config.ts
@@ -1,3 +1,4 @@
 export const APIKEY = process.env.NEXT_PUBLIC_API_KEY
-export const BASE_API_URL =
-  (process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000") + "/api"
+export const BASE_API_URL = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
+  : `http://localhost:3000/api`
