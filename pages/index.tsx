d50183c2bb94d556674d6bebad8292c51d385be5
Sigit Prabowo <sigit.prabowo@go-jek.com>
2021-08-02 09:38:15 +0700

    adding title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
-import { ClapButton, Provider, UpdownButton } from "@lyket/react"
+import Head from "next/head"
+import { ClapButton, UpdownButton } from "@lyket/react"
 
 const IndexPage = () => (
   <div className="w-full flex justify-center items-center bg-gray-100 min-h-screen relative">
     <div className="flex flex-wrap max-w-xl px-4 md:px-0">
+      <Head>
+        <title>Duyulaikit demo</title>
+      </Head>
       {/* <div className="w-full bg-white shadow-xl rounded-lg p-4 mb-4 last:mb-0">
-          <h2 className="font-semibold text-lg mb-2">Blog post #1</h2>
-          <p className="mb-2">
-            Yeay this is my first post. please support me, hit like below
-          </p>
-          <LikeButton id="artikel-2" namespace="demo" />
-        </div> */}
+        <h2 className="font-semibold text-lg mb-2">Blog post #1</h2>
+        <p className="mb-2">
+          Yeay this is my first post. please support me, hit like below
+        </p>
+        <LikeButton id="artikel-2" namespace="demo" />
+      </div> */}
       <div className="w-full bg-white shadow-xl rounded-lg p-4 mb-4 last:mb-0">
         <h2 className="font-semibold text-lg mb-2">Blog post #2</h2>
         <p className="mb-2">I'm happy to launch my second post. clap it here</p>
